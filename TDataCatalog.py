d29ea17855dadffd3b13b3aec4d896ffd11a1d38
Chris Klopp <chriseklopp@gmail.com>
2022-01-09 23:18:03 -0500

    Made minor changes. Plans to switch to SQL database in the future

--- TDataCatalog.py
+++ TDataCatalog.py
@@ -3,14 +3,19 @@ Singleton
 This class will manage the catalog of items and their properties
 Will initialize from a file containing the necessary information.
 
+
+# TODO: Switch over to using a SQLite database instead of dataframe.
 """
 import os
 import cv2
+import numpy
 import numpy as np
 import pandas as pd
 from dataclasses import dataclass, field
 import TItemTypes
 from typing import Union
+
+
 
 
 class TDataCatalog:
@@ -44,7 +49,7 @@ class TDataCatalog:
     def compare_to_catalog(self, comparate: TItemTypes.TItem) -> Union[TItemTypes.TItem, None]:
         for module in self.modules:
             for item in module.item_list:
-                match = item.compare_to(comparate)
+                match = comparate.compare_to(item)
                 if match:
                     return match
         # if no match is found in catalog, return None
@@ -78,13 +83,29 @@ class DataModule:
     def _create_items(self, df_row):
         index_names = df_row.index
         image_path = df_row["Image_path"]
-        if type(image_path) is str:
-            image = cv2.imread(image_path)
-        else:
-            image = np.zeros((64, 64, 3), np.uint8)  # used as default if image dir or specific image not found
 
+        print('--------------')
+        print(os.path.exists(image_path))
+        print(os.path.isfile(image_path))
+
+        if type(image_path) is str:
+
+            stream = open(image_path, 'rb')
+            bytes = bytearray(stream.read())
+            numpyarray = numpy.asarray(bytes, dtype=numpy.uint8)
+            image = cv2.imdecode(numpyarray, cv2.IMREAD_UNCHANGED)
+
+            npin = np.fromfile(image_path, dtype=np.uint8)
+            image = cv2.imdecode(npin, cv2.IMREAD_UNCHANGED)
+
+            # image = cv2.imread(image_path)
+
+        else:
+            image = np.ones((64, 64, 3), np.uint8)  # used as default if image dir or specific image not found
+            print(f'{df_row["Name"]} IMAGE NOT FOUND.')
         if type(image) is not np.ndarray:
-            image = np.zeros((64, 64, 3), np.uint8)  # used as default if image dir or specific image not found
+            image = np.ones((64, 64, 3), np.uint8)  # used as default if image dir or specific image not found
+            print(f'{df_row["Name"]} IMAGE NOT FOUND.')
 
         if "Inner_dims" in index_names:  # this means its some sort of container type object.
             # TODO: will have to support irregular container object, when I get around to implementing that
@@ -123,8 +144,8 @@ class DataModule:
             return int(i), int(j)
         return None
 
-
 if __name__ == "__main__":  # debug purposes, will generate the catalog for testing without doing anything else.
+
     x = TDataCatalog()
     # an = TItemTypes.TItem("ItemName", 1, (1, 1), False)
     # container = TItemTypes.TContainerItem("ContainerName", 1, (1, 1), False, (5, 5))
@@ -133,6 +154,5 @@ if __name__ == "__main__":  # debug purposes, will generate the catalog for test
     test_image = cv2.imread(r"C:\pyworkspace\tarkovinventoryproject\Data\testcompare\clipped.PNG")
     test_item = TItemTypes.TItem("My_Favorite_Helmet", test_image, (2, 2), False)
     result = x.compare_to_catalog(test_item)
-
     print()
 
