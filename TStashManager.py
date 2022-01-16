ebc7892670abe9ddf4ad79805880811f6774bde2
Chris Klopp <chriseklopp@gmail.com>
2022-01-15 20:53:46 -0500

    Added some preliminary code, doesn't do anythign yet

--- TStashManager.py
+++ TStashManager.py
@@ -8,8 +8,21 @@ Will contain a singleton TStash object and all items within it
 Will also call for reading of images by TImageReader and receives its output.
 Able to resolve conflicts between images and piece together an accurate recreation of a Stash.
 """
+import TItemTypes
+import TImageReader
+import TDataCatalog
 
 
 class TStashManager:
     def __init__(self):
-        pass
+        self.stash = TItemTypes.stash()
+
+
+if __name__ == "__main__":
+    # Debug purposes.
+    catalog = TDataCatalog.TDataCatalog()
+    reader = TImageReader.TImageReader()
+    global DataCatalog  # type: TDataCatalog.TDataCatalog
+    DataCatalog = TDataCatalog.TDataCatalog()
+    reader.run()
+
